ecce91e0c21d32a3d852453f5d2cf6563c89e0d4
Elias <ecjr22.ec@gmail.com>
2020-05-12 16:54:53 -0400

    first commit

---
+++ src/main/java/com/carmona/springdemo/dao/CustomerDAO.java
@@ -0,0 +1,17 @@
+package com.carmona.springdemo.dao;
+
+import java.util.List;
+
+import com.carmona.springdemo.entity.Customer;
+
+public interface CustomerDAO {
+
+	public List<Customer> getCustomers();
+
+	public void saveCustomer(Customer theCustomer);
+
+	public Customer getCustomer(int theId);
+
+	public void deleteCustomer(int theId);
+	
+}
---
+++ src/main/webapp/index.jsp
@@ -0,0 +1,10 @@
+<html>
+<body>
+
+<h3>Springboot REST API</h3>
+
+<hr>
+
+
+</body>
+</html>
