628a7615eb7304810b249e76b6fe8b14bf7fdf38
Elias <ecjr22.ec@gmail.com>
2020-05-22 15:44:48 -0400

    initial commit for spring security branch

---
+++ src/main/java/com/carmona/springdemo/controller/MainController.java
@@ -0,0 +1,15 @@
+package com.carmona.springdemo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class MainController {
+
+	
+	@GetMapping("/")
+	public String showHomePage() {
+		
+		return "home";
+	}
+}
