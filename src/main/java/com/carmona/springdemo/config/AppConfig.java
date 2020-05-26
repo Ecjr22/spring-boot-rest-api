4cce6f198c9a7c8fa8aa02a9d8447049ea9eaadc
Elias <ecjr22.ec@gmail.com>
2020-05-26 17:42:31 -0400

    updates to securityConfig

--- src/main/java/com/carmona/springdemo/config/AppConfig.java
+++ src/main/java/com/carmona/springdemo/config/AppConfig.java
@@ -1,19 +1,16 @@
 package com.carmona.springdemo.config;
 
 import java.beans.PropertyVetoException;
-import java.util.Properties;
 import java.util.logging.Logger;
 
 import javax.sql.DataSource;
 
-import org.hibernate.SessionFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.core.env.Environment;
-import org.springframework.orm.hibernate5.HibernateTransactionManager;
 import org.springframework.orm.hibernate5.LocalSessionFactoryBean;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 import org.springframework.web.servlet.ViewResolver;
@@ -78,22 +75,9 @@ public class AppConfig implements WebMvcConfigurer {
 
 		return myDataSource;
 	}
-	
-	private Properties getHibernateProperties() {
-
-		// set hibernate properties
-		Properties props = new Properties();
-
-		props.setProperty("hibernate.dialect", env.getProperty("hibernate.dialect"));
-		props.setProperty("hibernate.show_sql", env.getProperty("hibernate.show_sql"));
-		
-		return props;				
-	}
 
 	
-	// need a helper method 
-	// read environment property and convert to int
-	
+	// need a helper method to read environment property and convert to int	
 	private int getIntProperty(String propName) {
 		
 		String propVal = env.getProperty(propName);
@@ -112,22 +96,9 @@ public class AppConfig implements WebMvcConfigurer {
 		
 		// set the properties
 		sessionFactory.setDataSource(myDataSource());
-		sessionFactory.setPackagesToScan(env.getProperty("hibernate.packagesToScan"));
-		sessionFactory.setHibernateProperties(getHibernateProperties());
 		
 		return sessionFactory;
 	}
-	
-	@Bean
-	@Autowired
-	public HibernateTransactionManager transactionManager(SessionFactory sessionFactory) {
-		
-		// setup transaction manager based on session factory
-		HibernateTransactionManager txManager = new HibernateTransactionManager();
-		txManager.setSessionFactory(sessionFactory);
-
-		return txManager;
-	}	
 	
 }
 
--- src/main/java/com/carmona/springdemo/config/SecurityConfig.java
+++ src/main/java/com/carmona/springdemo/config/SecurityConfig.java
@@ -1,26 +1,27 @@
 package com.carmona.springdemo.config;
 
+import javax.sql.DataSource;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.User.UserBuilder;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
+	
+	@Autowired
+	private DataSource securityDataSource;
 
 	@Override
 	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
 		
-		UserBuilder users = User.withDefaultPasswordEncoder();
+		// spring security to connect to database
+		auth.jdbcAuthentication().dataSource(securityDataSource);
 		
-		auth.inMemoryAuthentication()
-			.withUser(users.username("John").password("test123").roles("EMPLOYEE"))
-			.withUser(users.username("Mary").password("test123").roles("EMPLOYEE", "MANAGER"))
-			.withUser(users.username("Susan").password("test123").roles("EMPLOYEE", "ADMIN"));
 	}
 
 	// reference custom login form
